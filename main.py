7a5600f88acbaecd0fbfac8b9eba9af0600c45db
jmjbs05 <josebarreto5988@gmail.com>
2025-01-27 20:49:18 +0100

    haha

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ titanic = pd.read_csv("titanic.csv")
 
 st.title("Titanic, Supervivientes:")
 
-st.table(data=titanic[["Age", "Sex"]])
+#st.table(data=titanic[["Age", "Sex"]])
+st.bar_chart(titanic[["Age", "Sex"]])
 
 """
  Objetivo: Usando https://docs.streamlit.io/develop/api-reference crear 3 elementos para mostrar datos de Titanic.
